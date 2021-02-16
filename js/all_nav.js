7fd8147f63bedbdbd978c91e21bc8c25b847609e
csa7770 <76501221+csa7770@users.noreply.github.com>
2021-02-16 15:38:24 +0900

    210216_15:38

--- js/all_nav.js
+++ js/all_nav.js
@@ -1,11 +1,14 @@
 $(function(){
     $(function(){
-     $('.category').hover(function(){
+     $('.category').mouseenter(function(){
        $('#all_nav').show();
        });
 
+       $('.category').mouseout(function(){
+        $('#all_nav').hide();
+       });
+
 // 오버 안 할 경우 사라지는 설정 해야함
-    
 
     })
 })
